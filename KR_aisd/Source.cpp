83450a08c075c93b67e7cca857d142df777e6cb8
Cmoild <cold17748@gmail.com>
2023-12-17 02:12:08 +0300

    creating class of hash table

--- KR_aisd/Source.cpp
+++ KR_aisd/Source.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
+template<typename T, typename Y>
+class HashTable {
+public:
+	class Node {
+		T key;
+		Y data;
+		bool state;
+		Node(T k, Y d) {
+			key = k;
+			data = d;
+			state = true;
+		}
+	};
+private:
+	Node* arr;
+	int size;
+	int buffer_size;
+	int size_all_non_nullptr;
+public:
+	int hash_func(string s) {
+		int h = 0, n = 0;
+		for each (char c in s) {
 
+		}
+		return 0;
+	}
+
+	int hash_func(int k) {
+		return 0;
+	}
+
+};
+
+int main() {
+	
 }
